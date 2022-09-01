d6a0b8d0fc56d0b882c7e6cecd5d1fd2dbe99d0e
douglas oliveira <douglasgama0@gmail.com>
2022-09-01 12:12:36 -0300

    feat: criando tabela para listar contatos

--- src/frontend/src/pages/contacts/Contacts.jsx
+++ src/frontend/src/pages/contacts/Contacts.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <div>
+      <div>
+        <h3>Meus Contatos</h3>
+        <input placeholder="filtrar contato" />
+        <button type="button">Adicionar Contato</button>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Nome</th>
+              <th>WhatsApp</th>
+              <th>E-mail</th>
+              <th>Ações</th>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>doug</td>
+              <td>999999</td>
+              <td>doug@email.com</td>
+              <td>WDE</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
