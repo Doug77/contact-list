376c7855b252f90c5a77742736209e868c058bfd
douglas oliveira <douglasgama0@gmail.com>
2022-09-02 15:30:53 -0300

    feat: criando primeira parte do index

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 
+const routes = require('./routes/routes');
+
 const app = express();
 const { PORT } = process.env;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('oi'));
+app.get('/', (_req, res) => res.send('oi'));
+
+app.use('/login', routes.userRouter);
